0a37ad7474e1d03402b95d3f5f5f4302af31ead9
Seirai Haraguchi <chihuyou90@gmail.com>
2026-01-11 14:11:00 +0800

    レ

--- check_icons.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from qfluentwidgets import FluentIcon
-    print("FluentIcon dir:", dir(FluentIcon), file=sys.stderr)
-    for item in FluentIcon:
-        print(item.name, file=sys.stderr)
-except Exception as e:
-    print(f"Error: {e}", file=sys.stderr)
--- check_qfw_exports.py
+++
@@ -1,6 +0,0 @@
-import sys
-try:
-    import qfluentwidgets
-    print("qfluentwidgets dir:", dir(qfluentwidgets), file=sys.stderr)
-except Exception as e:
-    print(f"Error: {e}", file=sys.stderr)
